59645156c6d3a23827c413647a32cf043699464a
Daniel Warehime <daniel37@umbc.edu>
2017-11-14 15:55:22 +0100

    Added facebook login option, haven't completed changiner login screen to pop up window when working through fb yet.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114084839) do
+ActiveRecord::Schema.define(version: 20171114095930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 20171114084839) do
     t.string "business_name"
     t.integer "pay_per_hour"
     t.string "payment_currency", default: "€"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
