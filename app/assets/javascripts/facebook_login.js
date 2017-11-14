7b0e7dfcd56c47adefa66ff1e12ad7bbd1ac552e
Daniel Warehime <daniel37@umbc.edu>
2017-11-14 17:03:44 +0100

    small fix of fb login js

--- app/assets/javascripts/facebook_login.js
+++ app/assets/javascripts/facebook_login.js
@@ -28,7 +28,7 @@ function statusChangeCallback(response) {
 
   window.fbAsyncInit = function() {
   FB.init({
-    appId      : `${fbAppId}`,
+    appId      : fbAppId,
     cookie     : true,  // enable cookies to allow the server to access
                         // the session
     xfbml      : true,  // parse social plugins on this page
