ae73f0a9c31073aef04f44ca3c88d13e6fb1ffca
Jake Engel <jakeengel01@gmail.com>
2017-11-14 12:25:02 +0100

    Updated the models with job and job_contracts. Plus I added random seeds and made some in the database. Run rails db:seed in order to get random values into your databases!

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114084839) do
+ActiveRecord::Schema.define(version: 20171114102312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "job_contracts", force: :cascade do |t|
+    t.integer "status"
+    t.integer "price"
+    t.string "payment_unit"
+    t.integer "freelancer_rating"
+    t.boolean "negotiations"
+    t.bigint "job_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_id"], name: "index_job_contracts_on_job_id"
+    t.index ["user_id"], name: "index_job_contracts_on_user_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "skills"
+    t.text "description"
+    t.string "title"
+    t.string "level"
+    t.string "industry"
+    t.string "location"
+    t.integer "pay_min"
+    t.integer "pay_max"
+    t.string "starting_date"
+    t.string "ending_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,4 +70,6 @@ ActiveRecord::Schema.define(version: 20171114084839) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "job_contracts", "jobs"
+  add_foreign_key "job_contracts", "users"
 end
