15ada84e9757f6f35c6664b7a7deddf39a579b90
Jake Engel <jakeengel01@gmail.com>
2017-11-16 17:39:53 +0100

    Fixed ES6 5o ES5 because doesn't work.

--- app/assets/javascripts/negotiate_price_form.js
+++ app/assets/javascripts/negotiate_price_form.js
@@ -2,6 +2,6 @@ const negotiate = document.getElementById('negotiation_for_contract')
 const showGroup = document.getElementById('show-on-accept')
 
 
-negotiate.addEventListener("click", (event) => {
+negotiate.addEventListener("click", function(event) {
   showGroup.classList.toggle('hidden')
 });
