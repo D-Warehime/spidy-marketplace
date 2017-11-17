9a8f7ceb41ad51d8d3d4cf48061f382d199949af
Daniel Warehime <daniel37@umbc.edu>
2017-11-17 14:02:24 +0100

    Change the contract application page so if a user doesn't want to negotiate the price and payment unit on the contract is that which the poster set.

--- app/assets/javascripts/negotiate_price_form.js
+++ app/assets/javascripts/negotiate_price_form.js
@@ -1,7 +1,19 @@
 const negotiate = document.getElementById('negotiation_for_contract')
 const showGroup = document.getElementById('show-on-accept')
+// const apply = document.getElementById('apply_for_contract')
 
 
 negotiate.addEventListener("click", function(event) {
   showGroup.classList.toggle('hidden')
 });
+
+//   document.getElementById("contract_price").innerHTML = document.getElementById("price").innerHTML;
+// console.log(document.getElementById("contract_price").innerHTML)
+
+// apply.addEventListener("click", function(event) {
+//   if (showGroup.classList.contains('hidden')) {
+//     // console.log(document.getElementById("contract_price").innerHTML)
+//     document.getElementById("contract_payment-unit").innerHTML = document.getElementById("payment-unit").innerHTML;
+//     console.log(document.getElementById("contract_payment-unit").innerHTML)
+//   };
+// });
